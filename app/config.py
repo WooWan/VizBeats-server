df87e27d68c2c0af13fc098f0f03a0c6ee692838
Changwan <47740690+WooWan@users.noreply.github.com>
2023-08-21 23:43:49 +0900

    chore: remove unused codes

--- app/config.py
+++ app/config.py
@@ -1,14 +1,4 @@
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-# class Settings(BaseSettings):
-#     DATABASE_URL:str
-#     app_name: str = "Awesome API"
-#     admin_email: str
-#     items_per_user: int = 50
-#
-#     model_config = SettingsConfigDict(env_file=".env")
 
 
 def setup_cors(app):
